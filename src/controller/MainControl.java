2d3894762bb273a34f13e92a82038dd3a885d13a
samikshamodi <samikshamodi7@gmail.com>
2020-11-19 19:25:35 +0530

    cleanup

--- src/controller/MainControl.java
+++ src/controller/MainControl.java
@@ -10,86 +10,54 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class MainControl {
-    public void newPlayerS(ActionEvent actionEvent) throws IOException {
-        Parent root= FXMLLoader.load(getClass().getResource("/gui/NewPlayerScene.fxml"));
-        Scene scene=new Scene(root);
+    private void show(ActionEvent actionEvent, String s) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource(s));
+        Scene scene = new Scene(root);
 
         //This line gets stage information
-        Stage window=(Stage)((Node) actionEvent.getSource()).getScene().getWindow();
+        Stage window = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
 
         window.setScene(scene);
         window.show();
+    }
+
+    public void newPlayerS(ActionEvent actionEvent) throws IOException {
+        String s = "/gui/NewPlayerScene.fxml";
+        show(actionEvent, s);
     }
 
     public void exPlayerS(ActionEvent actionEvent) throws IOException {
-        Parent root= FXMLLoader.load(getClass().getResource("/gui/ExistingPlayerScene.fxml"));
-        Scene scene=new Scene(root);
-
-        //This line gets stage information
-        Stage window=(Stage)((Node) actionEvent.getSource()).getScene().getWindow();
-
-        window.setScene(scene);
-        window.show();
+        String s = "/gui/ExistingPlayerScene.fxml";
+        show(actionEvent, s);
     }
 
     public void viewLeaderboardS(ActionEvent actionEvent) throws IOException {
-        Parent root= FXMLLoader.load(getClass().getResource("/gui/viewLeaderboardScene.fxml"));
-        Scene scene=new Scene(root);
-
-        //This line gets stage information
-        Stage window=(Stage)((Node) actionEvent.getSource()).getScene().getWindow();
-
-        window.setScene(scene);
-        window.show();
+        String s = "/gui/viewLeaderboardScene.fxml";
+        show(actionEvent, s);
     }
 
     public void quitS(ActionEvent actionEvent) {
-        Stage window=(Stage)((Node) actionEvent.getSource()).getScene().getWindow();
-
+        Stage window = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
         window.close();
     }
 
     public void gameMenuS(ActionEvent actionEvent) throws IOException {
-        Parent root= FXMLLoader.load(getClass().getResource("/gui/GameMenu.fxml"));
-        Scene scene=new Scene(root);
-
-        //This line gets stage information
-        Stage window=(Stage)((Node) actionEvent.getSource()).getScene().getWindow();
-
-        window.setScene(scene);
-        window.show();
+        String s = "/gui/GameMenu.fxml";
+        show(actionEvent, s);
     }
 
     public void mainMenuS(ActionEvent actionEvent) throws IOException {
-        Parent root= FXMLLoader.load(getClass().getResource("/gui/MainMenu.fxml"));
-        Scene scene=new Scene(root);
-
-        //This line gets stage information
-        Stage window=(Stage)((Node) actionEvent.getSource()).getScene().getWindow();
-
-        window.setScene(scene);
-        window.show();
+        String s = "/gui/MainMenu.fxml";
+        show(actionEvent, s);
     }
 
     public void startGameS(ActionEvent actionEvent) throws IOException {
-        Parent root= FXMLLoader.load(getClass().getResource("/gui/StartGameScene.fxml"));
-        Scene scene=new Scene(root);
-
-        //This line gets stage information
-        Stage window=(Stage)((Node) actionEvent.getSource()).getScene().getWindow();
-
-        window.setScene(scene);
-        window.show();
+        String s = "/gui/StartGameScene.fxml";
+        show(actionEvent, s);
     }
 
     public void selectGameS(ActionEvent actionEvent) throws IOException {
-        Parent root= FXMLLoader.load(getClass().getResource("/gui/SelectGameScene.fxml"));
-        Scene scene=new Scene(root);
-
-        //This line gets stage information
-        Stage window=(Stage)((Node) actionEvent.getSource()).getScene().getWindow();
-
-        window.setScene(scene);
-        window.show();
+        String s = "/gui/SelectGameScene.fxml";
+        show(actionEvent, s);
     }
 }
