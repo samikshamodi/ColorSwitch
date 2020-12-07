22ec759531b3db7a270d5d470c229e379aa3b0f9
samikshamodi <samikshamodi7@gmail.com>
2020-12-07 23:03:53 +0530

    game cleanup 1.0

--- src/colorswitch/Game.java
+++ src/colorswitch/Game.java
@@ -27,7 +27,12 @@ public class Game {
     ArrayList<ColorSwitcher> colorSwitchers;
     Ball ball;
     static int currentScore;
-    Game(){
+    int i = 0;
+    int N = 8;
+    Label score;
+    Button pause;
+
+    Game() {
         currentScore = 0;
         ball = new Ball(10, 0);
         obstacles = new ArrayList<>();
@@ -41,10 +46,12 @@ public class Game {
 
     void createObstacles() {
         for (int i = 0; i < 2; i++) {
-            obstacles.add(new Square("ty",1,1,1));
-            obstacles.add(new Circle("ty", 1, 1, 1));
-            obstacles.add(new Triangle("ty", 1, 1, 1));
-            obstacles.add(new Special("ty", 1, 1));
+            obstacles.add(new Square("square", 1, 1, 1));
+            obstacles.add(new Circle("circle", 1, 1, 1));
+            obstacles.add(new SpecialCircle("doubleCircle", 1, 1, 1));
+            obstacles.add(new Triangle("triangle", 1, 1, 1));
+            obstacles.add(new Plus("doublePlus", 1, 1, 1));
+            obstacles.add(new Special("plus", 1, 1));
         }
     }
 
@@ -63,8 +70,8 @@ public class Game {
 
 
     void createStars() {
-        for (int i = 0; i < 8; i++) {
-            stars.add(i,new Star(1, 1, 1));
+        for (int i = 0; i < N; i++) {
+            stars.add(new Star(1, 1, 1));
         }
     }
 
@@ -77,7 +84,7 @@ public class Game {
     }
 
     void createColorSwitchers() {
-        for (int i = 0; i < 8; i++) {
+        for (int i = 0; i < N; i++) {
             colorSwitchers.add(new ColorSwitcher(1, 1));
         }
     }
@@ -87,34 +94,32 @@ public class Game {
         colorSwitchers.get(1).appear(root);
         colorSwitchers.get(2).appear(root);
         colorSwitchers.get(0).setLayoutY(50);
-        colorSwitchers.get(2).setLayoutY(-3*700);
+        colorSwitchers.get(2).setLayoutY(-3 * 700);
     }
 
 
     public int startGame(Stage stage) throws IOException {
-
         //Setting the game scene
         FXMLLoader loader = new FXMLLoader();
         loader.setLocation(Main.class.getResource("/gui/StartGameScene.fxml"));
         AnchorPane root = loader.load();
         Scene scene = new Scene(root);
         BackgroundImage myBI = new BackgroundImage(new Image("/assets/pauseButton.png", 85, 85, true, true), BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, BackgroundSize.DEFAULT);
-        Button pause = new Button("");
+        pause = new Button("");
         pause.setBackground(new Background(myBI));
         pause.setPrefHeight(85);
         pause.setPrefWidth(85);
         pause.setTranslateX(500);
         pause.setTranslateY(20);
 
-        Label score = new Label("0");
-        score.setFont(new Font("Comic Sans",74));
+        score = new Label("0");
+        score.setFont(new Font("Comic Sans", 74));
         score.setTextFill(Color.WHITE);
         score.prefWidth(140);
         score.prefHeight(90);
         score.setLayoutX(37);
         score.setLayoutY(9);
-        root.getChildren().addAll(pause,score);
-
+        root.getChildren().addAll(pause, score);
 
         //changing objects in scene
         addObstacles(root);
@@ -122,6 +127,17 @@ public class Game {
         addStars(root);
         addColorSwitchers(root);
 
+        playGame(stage, root);
+
+        //set Scene and show stage
+        stage.setTitle("Color Switch");
+        stage.setScene(scene);
+        stage.show();
+
+        return 0;
+    }
+
+    public void playGame(Stage stage, AnchorPane root) {
         root.addEventFilter(MouseEvent.MOUSE_PRESSED, new EventHandler<MouseEvent>() {
             int flag = 0;
 
@@ -135,8 +151,6 @@ public class Game {
                         flag = 1;
                     }
                     Timeline timeline = new Timeline(new KeyFrame(Duration.millis(17), new EventHandler<ActionEvent>() {
-                        int i = 0;
-
                         @Override
                         public void handle(ActionEvent t) {
                             //move the ball
@@ -155,89 +169,48 @@ public class Game {
                                 ball.stay();
                                 //System.out.println("reached middle of screen");
 
-
                                 //now move the obstacle down to give the illusion of screen moving down
-                                colorSwitchers.get(i%8).moveDown();
-                                colorSwitchers.get((i + 1)%8).moveDown();
-                                colorSwitchers.get((i + 2)%8).moveDown();
+                                colorSwitchers.get(i % 8).moveDown();
+                                colorSwitchers.get((i + 1) % 8).moveDown();
+                                colorSwitchers.get((i + 2) % 8).moveDown();
 
-                                stars.get(i%8).moveDown();
-                                stars.get((i + 1)%8).moveDown();
-                                stars.get((i + 2)%8).moveDown();
+                                stars.get(i % 8).moveDown();
+                                stars.get((i + 1) % 8).moveDown();
+                                stars.get((i + 2) % 8).moveDown();
 
-                                obstacles.get(i%8).moveDown();
-                                obstacles.get((i + 1)%8).moveDown();
-                                obstacles.get((i + 2)%8).moveDown();
+                                obstacles.get(i % 8).moveDown();
+                                obstacles.get((i + 1) % 8).moveDown();
+                                obstacles.get((i + 2) % 8).moveDown();
 
-                                if (obstacles.get(i%8).getLayoutY() >= 800) {
-                                    obstacles.get(i%8).disappear(root);
+                                /* if obstacles are not on screen anymore then remove them from roor*/
+                                if (obstacles.get(i % 8).getLayoutY() >= 800) {
+                                    obstacles.get(i % 8).disappear(root);
                                     i++;
-                                    obstacles.get((i + 2)%8).appear(root);
-                                    stars.get((i + 2)%8).appear(root);
-                                    colorSwitchers.get((i + 2)%8).appear(root);
+                                    obstacles.get((i + 2) % 8).appear(root);
+                                    stars.get((i + 2) % 8).appear(root);
+                                    colorSwitchers.get((i + 2) % 8).appear(root);
                                 }
 
                             }
 
+                            hitObstacle();
+                            collectColorSwitcher(root);
+                            collectStars(root, score);
 
-                            /*if return value is 1 that means the star was collected.
-                            if the return value is 0 that means the star was not collected*/
-                            // check if star collected
-                            int starCollected1 = stars.get(i%8).checkCollision(ball.getShape());
-                            int starCollected2 = stars.get((i + 1)%8).checkCollision(ball.getShape());
-                            int starCollected3 = stars.get((i + 2)%8).checkCollision(ball.getShape());
-                            if (starCollected1 == 1)
-                                stars.get(i%8).disappear(root);
-                            else if (starCollected2 == 1)
-                                stars.get((i + 1)%8).disappear(root);
-                            else if (starCollected3 == 1)
-                                stars.get((i + 2)%8).disappear(root);
-                            if (starCollected1 == 1 || starCollected2 == 1 || starCollected3 == 1) {
-                                collectStars(root,score);
-                            }
-
-
-                            /*if return value is 1 that means the color switcher was collected.
-                            if the return value is 0 that means the color switcher was not collected*/
-                            //check if color switcher collected
-                            int csCollected1 = colorSwitchers.get(i%8).checkCollision(ball.getShape());
-                            int csCollected2 = colorSwitchers.get((i + 1)%8).checkCollision(ball.getShape());
-                            int csCollected3 = colorSwitchers.get((i + 2)%8).checkCollision(ball.getShape());
-                            if (csCollected1 == 1 || csCollected2 == 1 || csCollected3 == 1) {
-                                System.out.println("Color Switcher Collected");
-                                if (csCollected1 == 1)
-                                    colorSwitchers.get(i%8).disappear(root);
-                                else if (csCollected2 == 1)
-                                    colorSwitchers.get((i + 1)%8).disappear(root);
-                                else if (csCollected3 == 1)
-                                    colorSwitchers.get((i + 2)%8).disappear(root);
-                                ball.setColor();
-                            }
-
-                            //check if collision
-                            //collisionDetected has value 1 if ball collides with the obstacle which is not of the
-                            //same colour as ball
-                            //else it returns 0
-                            int collisionDetected1 = obstacles.get(i%8).checkCollision(ball.getShape());
-                            int collisionDetected2 = obstacles.get((i + 1)%8).checkCollision(ball.getShape());
-                            int collisionDetected3 = obstacles.get((i + 2)%8).checkCollision(ball.getShape());
-                            if (collisionDetected1 == 1 || collisionDetected2 == 1 || collisionDetected3 == 1) {
-                                System.out.println("Collision detected");
-                                System.out.println("Game Over");
-                                //TODO add the game over menu
-                            }
                             //ensures obstacles are infinite
                             if (i == 4) {
-                                Collections.shuffle(obstacles.subList(0,4));
+                                Collections.shuffle(obstacles.subList(0, 4));
                             }
                         }
                     }));
+
+                    //TODO move up before ball jump
                     pause.setOnAction(e -> {
                         try {
                             timeline.pause();
-                            int ans=pause(stage);
-                            System.out.println("Received: "+ans);
-                            if(ans == 1){
+                            int ans = pauseGame(stage);
+                            System.out.println("Received: " + ans);
+                            if (ans == 1) {
                                 timeline.play();
                             }
                         } catch (IOException ioException) {
@@ -249,21 +222,14 @@ public class Game {
                 }
             }
         });
-
-        //set Scene and show stage
-        stage.setTitle("Color Switch");
-        stage.setScene(scene);
-        stage.show();
-
-        return 0;
     }
 
-    int pause(Stage stage) throws IOException {
+    int pauseGame(Stage stage) throws IOException {
         FXMLLoader loader = new FXMLLoader();
         loader.setLocation(Main.class.getResource("/gui/PauseGame.fxml"));
         AnchorPane root = loader.load();
         Scene scene = new Scene(root);
-        AtomicInteger flag= new AtomicInteger();
+        AtomicInteger flag = new AtomicInteger();
 
         //resume game
         BackgroundImage myBI = new BackgroundImage(new Image("/assets/resumeButton.png", 200, 200, true, true), BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, BackgroundSize.DEFAULT);
@@ -283,10 +249,12 @@ public class Game {
         restart.setMinHeight(50);
         restart.setLayoutX(100);
         restart.setLayoutY(400);
-        restart.setFont(new Font("Courier New Bold",40));
+        restart.setFont(new Font("Courier New Bold", 40));
         restart.setTextFill(Color.WHITE);
         restart.setStyle("-fx-background-color: black");
-        restart.setOnAction(e->{flag.set(2);});
+        restart.setOnAction(e -> {
+            flag.set(2);
+        });
 
         //save game
         Button save = new Button("Save Game");
@@ -294,12 +262,14 @@ public class Game {
         save.setMinHeight(50);
         save.setLayoutX(100);
         save.setLayoutY(500);
-        save.setFont(new Font("Courier New Bold",40));
+        save.setFont(new Font("Courier New Bold", 40));
         save.setTextFill(Color.WHITE);
         save.setStyle("-fx-background-color: black");
-        save.setOnAction(e->{flag.set(3);});
+        save.setOnAction(e -> {
+            flag.set(3);
+        });
 
-        root.getChildren().addAll(resume,restart,save);
+        root.getChildren().addAll(resume, restart, save);
         stage.setTitle("Color Switch");
         stage.setScene(scene);
         stage.show();
@@ -307,24 +277,67 @@ public class Game {
     }
 
     int hitObstacle() {
-        return 3;
-    }
-
-    int collectStars(AnchorPane root,Label score) {
-        root.getChildren().remove(score);
-        currentScore+=1;
-        score.setText(""+currentScore);
-        root.getChildren().add(score);
+        /*check if collision*/
+        int collisionDetected1 = obstacles.get(i % 8).checkCollision(ball.getShape());
+        int collisionDetected2 = obstacles.get((i + 1) % 8).checkCollision(ball.getShape());
+        int collisionDetected3 = obstacles.get((i + 2) % 8).checkCollision(ball.getShape());
+        if (collisionDetected1 == 1 || collisionDetected2 == 1 || collisionDetected3 == 1) {
+            System.out.println("Collision detected");
+            System.out.println("Game Over");
+            //TODO add the game over menu
+        }
         return 0;
     }
 
-    String collectColorSwitcher() {
+    int collectStars(AnchorPane root, Label score) {
+
+        /* check if star collected*/
+        int starCollected1 = stars.get(i % 8).checkCollision(ball.getShape());
+        int starCollected2 = stars.get((i + 1) % 8).checkCollision(ball.getShape());
+        int starCollected3 = stars.get((i + 2) % 8).checkCollision(ball.getShape());
+        if (starCollected1 == 1)
+            stars.get(i % 8).disappear(root);
+        else if (starCollected2 == 1)
+            stars.get((i + 1) % 8).disappear(root);
+        else if (starCollected3 == 1)
+            stars.get((i + 2) % 8).disappear(root);
+
+        /*if star was hit then update score*/
+        if (starCollected1 == 1 || starCollected2 == 1 || starCollected3 == 1) {
+            root.getChildren().remove(score);
+            currentScore += 1;
+            score.setText("" + currentScore);
+            root.getChildren().add(score);
+        }
+        return 0;
+    }
+
+    String collectColorSwitcher(AnchorPane root) {
+
+        /*check if color switcher collected*/
+        int csCollected1 = colorSwitchers.get(i % 8).checkCollision(ball.getShape());
+        int csCollected2 = colorSwitchers.get((i + 1) % 8).checkCollision(ball.getShape());
+        int csCollected3 = colorSwitchers.get((i + 2) % 8).checkCollision(ball.getShape());
+
+        //if colorswitcher collected
+        if (csCollected1 == 1 || csCollected2 == 1 || csCollected3 == 1) {
+            System.out.println("Color Switcher Collected");
+            if (csCollected1 == 1) {
+                ball.setColor(colorSwitchers.get(i % 8).generateColor(ball.getColor()));
+                colorSwitchers.get(i % 8).disappear(root);
+            } else if (csCollected2 == 1) {
+                ball.setColor(colorSwitchers.get(i % 8).generateColor(ball.getColor()));
+                colorSwitchers.get((i + 1) % 8).disappear(root);
+            } else if (csCollected3 == 1) {
+                ball.setColor(colorSwitchers.get(i % 8).generateColor(ball.getColor()));
+                colorSwitchers.get((i + 2) % 8).disappear(root);
+            }
+        }
         return " ";
     }
 
     int fall() {
         return 2;
     }
-
 
 }
